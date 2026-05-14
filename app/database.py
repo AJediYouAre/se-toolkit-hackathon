ae80bdb73dfa673d8f57bae9a778b15a9d633df0
Igor <naigral18@gmail.com>
2026-05-14 14:57:42 +0300

    Dockerize application: add Dockerfile, docker-compose with PostgreSQL, update database config

--- app/database.py
+++ app/database.py
@@ -1,11 +1,14 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./recipes.db"
+DATABASE_URL = os.environ.get("DATABASE_URL", "sqlite:///./recipes.db")
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+connect_args = {}
+if DATABASE_URL.startswith("sqlite"):
+    connect_args["check_same_thread"] = False
+
+engine = create_engine(DATABASE_URL, connect_args=connect_args)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
